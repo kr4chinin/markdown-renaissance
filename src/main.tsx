40c6e75f9e080d550b7cbe3b14d8903737fa5640
Ilya Kruchinin <kr4chinin@mail.ru>
2022-09-20 13:54:11 +0300

    initial preparations

---
+++ src/main.tsx
@@ -0,0 +1,7 @@
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import './index.scss'
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+	<App />
+)
---
+++ vite.config.ts
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import eslint from 'vite-plugin-eslint'
+import svgr from 'vite-plugin-svgr'
+
+export default defineConfig({
+	plugins: [react(), eslint(), svgr()]
+})
